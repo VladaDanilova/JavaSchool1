97f76080caf01f599c9c87ed81b5e00374d67c9c
Danilova Vlada <just.you177@gmail.com>
2021-03-10 19:34:18 +0300

    Subsequence

--- src/main/java/com/tsystems/javaschool/tasks/subsequence/Subsequence.java
+++ src/main/java/com/tsystems/javaschool/tasks/subsequence/Subsequence.java
@@ -1,10 +1,12 @@
 package com.tsystems.javaschool.tasks.subsequence;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Subsequence {
 
     /**
+     * @author vladadanilova
      * Checks if it is possible to get a sequence which is equal to the first
      * one by removing some elements from the second one.
      *
@@ -12,9 +14,32 @@ public class Subsequence {
      * @param y second sequence
      * @return <code>true</code> if possible, otherwise <code>false</code>
      */
-    @SuppressWarnings("rawtypes")
+
     public boolean find(List x, List y) {
-        // TODO: Implement the logic here
-        return false;
+
+            if (x == null || y == null)
+                throw new IllegalArgumentException();
+
+        if (x.size() > y.size()) //если Х длиннее, то уже сразу не подходит
+        {
+            return false;
+        }
+
+        int lenX = x.size();
+        int lenY = y.size();
+        int i = 0, j = 0;
+
+        while (i < lenX && j < lenY)
+        {
+            while ((j < lenY) && (x.get(i) != y.get(j)))
+            {
+                j++;
+            }
+            if (j == lenY)
+                break;
+            i++;
+            j++;
+        }
+        return i == lenX;
     }
 }
